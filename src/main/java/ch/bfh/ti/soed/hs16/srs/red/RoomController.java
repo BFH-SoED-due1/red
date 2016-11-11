ab445938347f733d8aee27976b5a4559b90f217f
tambur <tamara_burri@hotmail.com>
2016-11-11 11:20:39 +0100

    create class that generates an overview of all existing classes

---
+++ src/main/java/ch/bfh/ti/soed/hs16/srs/red/RoomController.java
@@ -0,0 +1,66 @@
+package ch.bfh.ti.soed.hs16.srs.red;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author tambur
+ */
+public class RoomController {
+
+    //TODO Implement Database
+
+
+    private List<Room> roomOverview;
+
+
+    public List<Room> getRooms() {
+        return roomOverview;
+    }
+
+    public void setRoomOverview(List<Room> roomOverview) {
+        this.roomOverview = roomOverview;
+    }
+
+
+    /**
+     * Adding a room to the List of all existing rooms
+     * @param room the room to add to the list
+     * @return tells you if success. -1 if room is null or already exists and 1 if adding was successful
+     */
+    public int addRoom(Room room) {
+        if(roomOverview == null) {
+            this.roomOverview = new ArrayList<>();
+        }
+        if (room != null && roomOverview != null) {
+            for(Room r : roomOverview) {
+                if(r.getName().equals(room.getName())) return -1; // return -1 => room already exist or null
+            }
+            roomOverview.add(room);
+            return 1; //return 1 => success
+        } else {
+            return -1; //nothing happens return -1 => room already exist or null
+        }
+    }
+
+
+    /**
+     * Remove a room from the List of all existing romms
+     * @param room the room to be removed
+     */
+    public void removeRoom(Room room) {
+
+        if (room != null) {
+            for (Room r : roomOverview) {
+                if (r.getName().equals(room.getName())) {
+                    roomOverview.remove(roomOverview.indexOf(r));
+                    break;
+                } else {
+                    //nothing happens
+                }
+            }
+        }
+
+    }
+
+}
---
+++ src/test/java/ch/bfh/ti/soed/hs16/srs/red/RoomControllerTest.java
@@ -0,0 +1,157 @@
+package ch.bfh.ti.soed.hs16.srs.red;
+
+import org.junit.Test;
+
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+/**
+ * Created by scarface on 10.11.2016.
+ */
+public class RoomControllerTest {
+
+
+    List<Room> roomOverviewTest = new ArrayList<>();
+
+
+    /**
+     * Test if correct room has been removed
+     */
+    @Test
+    public void testRemoveRoom() {
+
+        //Creates 10 rooms and add it to List
+        for(int i=1; i<=10; i++) {
+            roomOverviewTest.add(new Room("room"+i, "building"+i, i));
+        }
+
+        RoomController roomController = new RoomController();
+        roomController.setRoomOverview(roomOverviewTest);
+
+        Room r6 = new Room("room6", "building6", 6);
+        roomController.removeRoom(r6);
+
+        List<Room> room = roomController.getRooms();
+
+        // Creates second List without deleted room
+        List<Room> roomWithoutRoom6 = new ArrayList<>();
+        roomWithoutRoom6.add(new Room("room1","building1", 1));
+        roomWithoutRoom6.add(new Room("room2","building2", 2));
+        roomWithoutRoom6.add(new Room("room3","building3", 3));
+        roomWithoutRoom6.add(new Room("room4","building4", 4));
+        roomWithoutRoom6.add(new Room("room5","building5", 5));
+        roomWithoutRoom6.add(new Room("room7","building7", 7));
+        roomWithoutRoom6.add(new Room("room8","building8", 8));
+        roomWithoutRoom6.add(new Room("room9","building9", 9));
+        roomWithoutRoom6.add(new Room("room10","building10", 10));
+
+        assertEquals(9, room.size());
+        assertEquals(9, roomWithoutRoom6.size());
+
+        for(int i=0; i<roomController.getRooms().size(); i++) {
+            assertEquals(roomController.getRooms().get(i).getName(), roomWithoutRoom6.get(i).getName());
+        }
+    }
+
+    /**
+     * Test if nothing happens when room isn't in the list or room == null
+     */
+    @Test
+    public void testRemoveRoomNotExists() {
+
+        //Creates 10 rooms and add it to List
+        for(int i=1; i<=10; i++) {
+            roomOverviewTest.add(new Room("room"+i, "building"+i, i));
+        }
+
+        RoomController roomController = new RoomController();
+        roomController.setRoomOverview(roomOverviewTest);
+
+        Room r6 = new Room("TestRoom", "TestBuilding", 45);
+
+        RoomController roomController1 = new RoomController();
+        roomController1.setRoomOverview(roomOverviewTest);
+        Room r7 = null;
+
+        roomController.removeRoom(r6);
+        roomController1.removeRoom(r7);
+
+        List<Room> room = roomController.getRooms();
+        List<Room> room1 = roomController1.getRooms();
+
+        assertEquals(10, room.size());
+        assertEquals(10, room1.size());
+    }
+
+
+    /**
+     * Test add new Room to the list
+     */
+    @Test
+    public void testAddRoom() {
+
+        //Creates 10 rooms and add it to List
+        for(int i=1; i<=10; i++) {
+            roomOverviewTest.add(new Room("room"+i, "building"+i, i));
+        }
+
+        RoomController roomController = new RoomController();
+        roomController.setRoomOverview(roomOverviewTest);
+
+        Room r6 = new Room("newRoom", "newBuilding", 34);
+
+        roomController.addRoom(r6);
+        List<Room> roomList = roomController.getRooms();
+
+        assertEquals(11, roomList.size());
+    }
+
+    /**
+     * Test add Rooms with same name or null element
+     */
+    @Test
+    public void testAddRoomWithSameName() {
+
+        //Creates 10 rooms and add it to List
+        for(int i=1; i<=10; i++) {
+            roomOverviewTest.add(new Room("room"+i, "building"+i, i));
+        }
+
+        RoomController roomController = new RoomController();
+        roomController.setRoomOverview(roomOverviewTest);
+
+        Room r6 = new Room("room6", "building6", 6);
+
+        roomController.addRoom(r6);
+        List<Room> roomList = roomController.getRooms();
+
+        assertEquals(10, roomList.size());
+
+    }
+
+
+    /**
+     * Test add Rooms when list isn't initialized
+     */
+    @Test
+    public void testAddRoomWithListNull() {
+
+        RoomController roomController = new RoomController();
+
+        Room r6 = new Room("room6", "building6", 6);
+
+        roomController.addRoom(r6);
+
+        assertEquals(1, roomController.getRooms().size());
+
+
+        //Test
+
+    }
+
+}
