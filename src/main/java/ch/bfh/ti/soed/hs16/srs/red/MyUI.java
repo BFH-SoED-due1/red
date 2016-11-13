81242fadbc10bdd7970ae81a168d0c1cdb70da75
tambur <tamara_burri@hotmail.com>
2016-11-13 22:13:17 +0100

    reformatted code

--- src/main/java/ch/bfh/ti/soed/hs16/srs/red/MyUI.java
+++ src/main/java/ch/bfh/ti/soed/hs16/srs/red/MyUI.java
@@ -9,7 +9,12 @@ import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.*;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Label;
+
 
 import javax.servlet.annotation.WebServlet;
 
