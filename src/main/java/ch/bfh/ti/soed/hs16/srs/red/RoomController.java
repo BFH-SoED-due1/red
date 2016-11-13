0717c09b1c69e68cf6899c79fa4e31fc23828d44
tambur <tamara_burri@hotmail.com>
2016-11-13 22:13:54 +0100

    improve and reformatted code

--- src/main/java/ch/bfh/ti/soed/hs16/srs/red/RoomController.java
+++ src/main/java/ch/bfh/ti/soed/hs16/srs/red/RoomController.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2016 Berner Fachhochschule, Switzerland.
+ * Project Smart Reservation System.
+ * Distributable under GPL license. See terms of license at gnu.org.
+ */
 package ch.bfh.ti.soed.hs16.srs.red;
 
 import java.util.ArrayList;
@@ -25,27 +30,32 @@ public class RoomController {
 
     /**
      * Adding a room to the List of all existing rooms
+     *
      * @param room the room to add to the list
      * @return tells you if success. -1 if room is null or already exists and 1 if adding was successful
      */
     public int addRoom(Room room) {
-        if(roomOverview == null) {
+
+        if (roomOverview == null) {
             this.roomOverview = new ArrayList<>();
         }
-        if (room != null && roomOverview != null) {
-            for(Room r : roomOverview) {
-                if(r.getName().equals(room.getName())) return -1; // return -1 => room already exist or null
-            }
+
+        if (room != null) {
+
+            for (Room r : roomOverview)
+                if (r.getName().equals(room.getName())) return -1; // return -1 => room already exist or null
+
             roomOverview.add(room);
             return 1; //return 1 => success
-        } else {
-            return -1; //nothing happens return -1 => room already exist or null
         }
+
+        return -1; //nothing happens return -1 => room already exist or null
     }
 
 
     /**
      * Remove a room from the List of all existing romms
+     *
      * @param room the room to be removed
      */
     public void removeRoom(Room room) {
