82ca3def175de59b18e8c5038cc5d3e6bb674821
tambur <tamara_burri@hotmail.com>
2016-12-04 21:13:51 +0100

    create new classes for view

---
+++ src/main/java/ch/bfh/ti/soed/hs16/srs/red/ui/helper/Menu.java
@@ -0,0 +1,7 @@
+package ch.bfh.ti.soed.hs16.srs.red.ui.helper;
+
+/**
+ * Created by scarface on 04.12.2016.
+ */
+public class Menu {
+}
---
+++ src/main/java/ch/bfh/ti/soed/hs16/srs/red/ui/views/LoginView.java
@@ -0,0 +1,7 @@
+package ch.bfh.ti.soed.hs16.srs.red.ui.views;
+
+/**
+ * Created by scarface on 04.12.2016.
+ */
+public class LoginView {
+}
