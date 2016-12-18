f70e3dd1b11f17da2771ed0d66ae994df2b24e6a
tambur <tamara_burri@hotmail.com>
2016-12-18 23:34:44 +0100

    improve view

--- src/main/java/ch/bfh/ti/soed/hs16/srs/red/ui/views/LoginView.java
+++ src/main/java/ch/bfh/ti/soed/hs16/srs/red/ui/views/LoginView.java
@@ -5,29 +5,27 @@
  */
 package ch.bfh.ti.soed.hs16.srs.red.ui.views;
 
-import ch.bfh.ti.soed.hs16.srs.red.service.UserController;
+//TODO clean imports
 import com.vaadin.navigator.Navigator;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.CustomComponent;
-import com.vaadin.ui.GridLayout;
 import com.vaadin.ui.Label;
-import com.vaadin.ui.Panel;
 import com.vaadin.ui.PasswordField;
 import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
 
 /**
  * Created by tambur on 04.12.2016.
  */
 public class LoginView extends CustomComponent implements View {
 
-    private Button loginButton;
+
+    private VerticalLayout vertical;
     private TextField loginName;
     private PasswordField passwordField;
-    private GridLayout layout;
-    private Panel root;
-    private UserController userController;
+    private Button loginButton;
     private Label error;
 
     public LoginView(Navigator nav) {
@@ -35,11 +33,11 @@ public class LoginView extends CustomComponent implements View {
         /*---------------------------------
         initalize Objects
         ---------------------------------*/
-        this.loginButton = new Button("Login");
+        this.vertical = new VerticalLayout();
         this.loginName = new TextField("username");
         this.passwordField = new PasswordField("password");
-        this.layout = new GridLayout(6,6);
-        this.root = new Panel();
+        this.loginButton = new Button("Login");
+
         //this.userController = new UserController();
         //TODO: handle with user controller
         this.error = new Label();
@@ -47,6 +45,7 @@ public class LoginView extends CustomComponent implements View {
         /*-------------------------------
         add to css
         -------------------------------*/
+        vertical.setPrimaryStyleName("rootLogin");
         loginButton.setStyleName("buttonLogin");
         error.setPrimaryStyleName("labelLogin");
 
@@ -54,27 +53,13 @@ public class LoginView extends CustomComponent implements View {
         /*-------------------------------
         add Components to Layout
         --------------------------------*/
-        layout.addComponent(loginName, 2,0,3,0);
-        layout.addComponent(passwordField, 2,1,3,1);
-        layout.addComponent(loginButton, 2,2,3,2);
-        layout.addComponent(error, 2,3,3,3);
-
-        root.setContent(layout);
-        root.setSizeFull();
-        setCompositionRoot(root);
+        vertical.addComponents(loginName, passwordField, loginButton, error);
+        setCompositionRoot(vertical);
 
 
-
-        loginButton.addClickListener(new Button.ClickListener() {
-
-            @Override
-            public void buttonClick(Button.ClickEvent clickEvent) {
-
-                //TODO: handle Login
-                nav.navigateTo("overview");
-
-
-            }
+        loginButton.addClickListener((Button.ClickListener) clickEvent -> {
+            //TODO: handle Login
+            nav.navigateTo("my Reservation");
         });
     }
 
